78f9bbd4d4b7c003985c7fc3263b4592afe9fdf4
Nathan Humphrey <NathanH@nait.ca>
2023-10-23 21:23:41 -0600

    Add WestWind projects

---
+++ CPSC1517Workbook/WestWindConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Headers;
+using WestWindSystem.DAL;
+
+var db = new WestWindContext();
+
+var countCustomers = db.Customers.Count();
+
+Console.WriteLine($"There are {countCustomers} customers in the db.");
+
+var customer = db.Customers.FirstOrDefault();
+
+Console.WriteLine($"The first customer is {customer.ContactName}");
