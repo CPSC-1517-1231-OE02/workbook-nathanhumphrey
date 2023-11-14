b3f6448662f153b6ff116d8c15acb63a1e1cfa15
Nathan Humphrey <NathanH@nait.ca>
2023-11-13 20:25:13 -0700

    Update WW Products Page

--- CPSC1517Workbook/WestWindSystem/BLL/ProductServices.cs
+++ CPSC1517Workbook/WestWindSystem/BLL/ProductServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,17 @@ namespace WestWindSystem.BLL
 
 		public List<Product> GetProductsByCategoryId(int id)
 		{
-			return _context.Products.Where(p => p.CategoryId == id).ToList<Product>();
+			return _context.Products.Where(p => p.CategoryId == id)
+				.Include(p => p.Supplier)
+				.ToList<Product>();
+		}
+
+		public List<Product> GetProductsByNameOrCategoryName(string partial)
+		{
+			partial = partial.ToLower();
+			return _context.Products.Where(p => p.ProductName.ToLower().Contains(partial) || p.Category.CategoryName.ToLower().Contains(partial))
+				.Include(p => p.Supplier)
+				.ToList<Product>();
 		}
 	}
 }
